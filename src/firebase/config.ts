f8d2de8f3ab13e2db910f62d0da370cc6d50522f
Igor Guilherme Custodio <igor.custodio8@gmail.com>
2025-12-12 22:15:29 +0000

    ok

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,11 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  authDomain: "safety-docs-ai-app.firebaseapp.com",
   projectId: "safety-docs-ai-app",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "safety-docs-ai-app.appspot.com",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
